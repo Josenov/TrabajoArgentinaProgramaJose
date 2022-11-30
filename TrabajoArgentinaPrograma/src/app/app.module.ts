8896c0348a1b9cac53d5a28a3cf322da6f5f1af5
Jose Maria Novillo <josenovillo88@gmail.com>
2022-11-30 12:00:06 -0300

    Creacion seccion Header + Acerca De

--- TrabajoArgentinaPrograma/src/app/app.module.ts
+++ TrabajoArgentinaPrograma/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoArgProgramaComponent } from './logo-arg-programa/logo-arg-programa.component';
+import { RedessocialesComponent } from './redessociales/redessociales.component';
+import { BannerComponent } from './banner/banner.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgramaComponent,
+    RedessocialesComponent,
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
